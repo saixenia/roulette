0d9535160261709c1a4e56c616addc028571bfdb
Julian Rodriguez <jarodriguez601@gmail.com>
2020-11-08 00:06:10 -0500

    initial config

---
+++ roulette/Controllers/RouletteController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace roulette.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RouletteController : ControllerBase
+    {
+        // GET: api/<RouletteController>
+        [HttpGet]
+        public int Get()
+        {
+            //create roulette
+            //return rouletteId;
+            return 1;
+        }
+
+        // GET api/<RouletteController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<RouletteController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+
+        }
+
+        // PUT api/<RouletteController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<RouletteController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ roulette/Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace roulette
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging =>
+                {
+                    logging.AddEventLog(eventLogSettings =>
+                    {
+                        eventLogSettings.SourceName = "MyLogs";
+                    });
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
